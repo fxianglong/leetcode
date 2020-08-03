d26e3cc8139297dfd0563c2e57304334ab00ebe2
fxianglong <1244190336@qq.com>
2020-08-03 22:39:14 +0800

    1402.做菜顺序

---
+++ 1402.做菜顺序.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	int maxSatisfaction(vector<int>& satisfaction) {
+		sort(satisfaction.begin(), satisfaction.end(), greater<int>());
+		int sum = 0, tmp = 0;
+		for (auto it : satisfaction){
+			if (tmp + it>0){
+				tmp += it;
+				sum += tmp;
+			}
+			else{
+				break;
+			}
+		}
+		return sum;
+	}
+};
