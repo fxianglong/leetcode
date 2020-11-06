05cc553dc2b17f98aeeec2835ce3bc2113e2b2c7
fxianglong <1244190336@qq.com>
2020-11-06 21:42:50 +0800

    1356.根据数字二进制下1的个数排序

---
+++ 1356.根据数字二进制下1的个数排序.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+vector<int> sortByBits(vector<int>& arr) {
+        vector<int> bit(10001, 0);
+        for (int i = 1;i <= 10000; ++i) {
+            bit[i] = bit[i>>1] + (i & 1);
+        }
+        sort(arr.begin(),arr.end(),[&](int x,int y){
+            if (bit[x] < bit[y]) {
+                return true;
+            }
+            if (bit[x] > bit[y]) {
+                return false;
+            }
+            return x < y;
+        });
+        return arr;
+    }
+};
