c70e8157c6199c21782cabb24a82f67e0d013a7d
fxianglong <1244190336@qq.com>
2021-02-20 21:41:31 +0800

    448. 找到所有数组中消失的数字

---
+++ 448. 找到所有数组中消失的数字.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> findDisappearedNumbers(vector<int>& nums) {
+		vector<int> res;
+		if (nums.empty()) return nums;
+		for (int i = 0; i<nums.size(); i++)
+		{
+			int index = (nums[i] - 1) % nums.size();
+			nums[index] += nums.size();
+		}
+		for (int i = 0; i<nums.size(); i++)
+		{
+			if (nums[i] <= nums.size())
+				res.push_back(i + 1);
+		}
+		return res;
+	}
+};
